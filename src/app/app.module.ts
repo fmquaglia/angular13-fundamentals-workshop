3025977bca5cabdbd3eb3a6c171d92b059c55327
fmquaglia <matias.quaglia@gmail.com>
2023-02-23 09:58:28 -0300

    Starting to work with template driven forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MaterialModule } from './material.module';
-import { HomeComponent } from './home/home.component';
-import { CoursesComponent } from './courses/courses.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MaterialModule} from './material.module';
+import {HomeComponent} from './home/home.component';
+import {CoursesComponent} from './courses/courses.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   imports: [
@@ -16,9 +17,11 @@ import { CoursesComponent } from './courses/courses.component';
     BrowserAnimationsModule,
     MaterialModule,
     HttpClientModule,
+    FormsModule
   ],
   declarations: [AppComponent, HomeComponent, CoursesComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
